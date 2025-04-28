cef9376a90d0c0debff60aa99c0904ca100192dd
vitamin1312 <vitamin20021312@gmail.com>
2025-04-28 21:10:43 +0300

    test commit

--- utils/metadataIO.py
+++ utils/metadataIO.py
@@ -1,6 +1,7 @@
 import json
 
 def load_json(file_path):
+    "Open json file and returns dict"
     with open(file_path, 'r', encoding='utf-8') as file:
         data = json.load(file)
     return data
