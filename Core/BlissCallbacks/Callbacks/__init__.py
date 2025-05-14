a9487abe47160a89bb66a92d1493918dd62335b1
vitamin1312 <vitamin20021312@gmail.com>
2025-05-14 04:05:04 +0300

    Init fix

--- Core/BlissCallbacks/Callbacks/__init__.py
+++ Core/BlissCallbacks/Callbacks/__init__.py
@@ -1,5 +1,6 @@
 from .BlissCallback import BlissCallback
 from .CustomBlissCallbacks import (
+    CommonMetricsCallback,
     SegmentationMetricsCallback,
     PrintCriteriaCallback,
     ColorizationMetricsCallback,
